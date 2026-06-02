25a79cffaf5a0e8c4d7e6a66ead9e8cd4d8b7fdf
ÉRICA PRESTES DE OLIVEIRA DA SILVA <erica_po_silva@estudante.sesisenai.org.br>
2026-06-02 15:53:37 -0300

    Criar personagem

---
+++ personagem.h
@@ -0,0 +1,21 @@
+ #pragma once 
+ #include <iostream>
+ #include <string>
+ 
+ class Personagem {
+public:
+ 	std:: string nome;
+ 	int vida;
+ 	int ataque;
+ 	
+ 	Personagem(std::string n, int v, int a)
+ 		:nome(n), vida(v), ataque (a) {}
+ 		
+ 	void apresentarn(){
+ 		std: cout << "Personagem: " << nome << std :: endl;
+ 		std:cout <<  "Vida: " << vida << std:: endl;
+ 		std:: cout << "Ataque: " << ataque << std:: endl;
+	 }
+	 
+	 
+ };
