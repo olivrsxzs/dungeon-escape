ca3568f1436342e9ec94931a2d721fc458939166
ÉRICA PRESTES DE OLIVEIRA DA SILVA <erica_po_silva@estudante.sesisenai.org.br>
2026-06-02 17:09:09 -0300

    Inimigo

--- main.cpp
+++ main.cpp
@@ -4,5 +4,9 @@
 int main () {
 	Personagem heroi("Herói", 100, 10);
 	heroi.apresentar(); 
+	
+	Inimigo goblin("Goblin", 30, 5, "Goblin");
+	goblin.apresentar();
+	
 	return 0;
 }
--- personagem.h
+++ personagem.h
@@ -11,11 +11,28 @@ public:
  	Personagem(std::string n, int v, int a)
  		:nome(n), vida(v), ataque (a) {}
  		
- 	void apresentarn(){
- 		std: cout << "Personagem: " << nome << std :: endl;
- 		std:cout <<  "Vida: " << vida << std:: endl;
+ 	void apresentar(){
+ 		std::cout << "Personagem: " << nome << std:: endl;
+ 		std::cout <<  "Vida: " << vida << std:: endl;
  		std:: cout << "Ataque: " << ataque << std:: endl;
 	 }
+	   
 	 
-	 
+ };
+ 
+ 
+ class Inimigo : public Personagem {
+ 	public:
+ 		std:: string tipo; 
+ 		
+ 		Inimigo(std:: string n, int v, int a, std:: string t)
+ 			:Personagem(n, v, a), tipo(t) {}
+ 			
+ 	void apresentar() {
+ 		std::cout << "[INIMIGO] " << nome
+ 				<< " | Tipo: " << tipo
+ 				<< " | Vida: " << vida
+ 				<< " | Ataque: " << ataque << std:: endl;
+	 }
+			 
  };
